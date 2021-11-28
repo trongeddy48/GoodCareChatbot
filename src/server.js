2f936fc708fc2688003f1845973b382dd81936c6
Nguyễn Thanh Trọng <trongeddy48@gmail.com>
2021-11-28 21:30:04 +0700

    update

--- src/server.js
+++ src/server.js
@@ -5,14 +5,15 @@ import webRoutes from './routes/web';
 
 let app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
 //config view engine
 viewEngine(app);
 
 //config web routes
 webRoutes(app);
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
 
 let port = process.env.PORT || 4000;
 
