c067ce5b5f1d178390e6c6c5f2c5a12abac392a7
Nguyễn Thanh Trọng <trongeddy48@gmail.com>
2021-11-28 22:33:52 +0700

    update

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -124,6 +124,8 @@ function handlePostback(sender_psid, received_postback) {
     response = { "text": "Thanks!" }
   } else if (payload === 'no') {
     response = { "text": "Oops, try sending another image." }
+  }else if(payload === "GET_STARTED"){
+    response = { "text": "Chào mừng bạn đã đến với GoodCare !" }
   }
   // Send the message to acknowledge the postback
   callSendAPI(sender_psid, response);
